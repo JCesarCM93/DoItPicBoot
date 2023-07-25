f1235c09dfc83a7bb012c36a634bd8f15394e5d1
Julio Mendoza <40074332+JCesarCM93@users.noreply.github.com>
2023-07-25 07:11:37 -0600

    Update main.s

--- PIC18F27Q83/LearnAssembler/01 UART_WREG.X/main.s
+++ PIC18F27Q83/LearnAssembler/01 UART_WREG.X/main.s
@@ -13,7 +13,7 @@ PSECT resetVec,space=2
 org 0x800
     CALL    InitSys
 LOOP:	
-    CALL    UartRead
-    ADDLW   1
-    Call    UartWrite
-    GOTO    LOOP
+    CALL    UartRead     // read data from serial and save in WREG
+    ADDLW   1            // add 1 unit of WREG
+    Call    UartWrite    // send value
+    GOTO    LOOP         // go to loop
